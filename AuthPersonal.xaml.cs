32c6a699b180e504a25ecadca0e5b901655b4224
Egor <kumiss220@gmail.com>
2021-10-28 21:22:08 +0300

    Добавьте файлы проекта.

---
+++ AuthPersonal.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Klinika_Karasev_3ISP11_12
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPersonal.xaml
+    /// </summary>
+    public partial class AuthPersonal : Page
+    {
+        public AuthPersonal()
+        {
+            InitializeComponent();
+        }
+    }
+}
